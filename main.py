87bd6324b5f24612a2f371b552449e84abda2244
faffnir68 <faffnir68@gmail.com>
2019-05-22 00:44:30 +0200

    4th commit

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
-#player1 = input("Please pick a marker 'X' or 'O'")
-#position = int(input("Please enter a number"))
-
-
-def display_board(board):
-    print('\n' * 100)
-    print('   |   |   ')
+# Créer la tableboard
+def board_game(board):
+    print('\n'*10)
+    print('   |   |')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |   ')
-    print("-----------")
-    print('   |   |   ')
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |   ')
-    print("-----------")
-    print('   |   |   ')
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |   ')
+    print('   |   |')
 
+table_board = ["#", "X", "O", "X", "X", "O", "X", "X", "O", "X"]
+board_game(table_board)
+# Créer les joueurs en laissant le premier joueur choisir X ou O
 
-test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
-display_board(test_board)
+def player_input():
+    player1 = input("Player 1: please choose between 'X' or 'O' by typing your choice :").upper()
+    if player1 == 'X':
+        player2 = 'O'
+        return player1, player2
+    elif player1 == 'O':
+        player2 = 'X'
+        return player1, player2
+    else:
+        player_input()
 
-def player_choose(player_mark_choice):
-    pass
+print(player_input())
 
-def player_play(board, player_position):
-    pass
+def place_marker()
 
-def player_win(board):
-    pass
+# Implémenter ce que joueur 1 a entré
+# Regarder si joueur 1 a gagné ou s'il y égalité
+# Passer à joueur 2
+# Demander à rejouer
